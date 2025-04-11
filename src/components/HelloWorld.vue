9f5bd0065d1f946fc66d9585e5fd847a37e1687f
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-04-12 05:02:39 +0530

    `feat`: implement basic routing and pages

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,61 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
 defineProps<{
   msg: string
+  tagline: string
 }>()
+
+const count = ref(0)
+const router = useRouter()
+const login = () => {
+  router.push('/login')
+}
+const join = () => {
+  router.push('/join')
+}
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+  <h1>{{ msg }}</h1>
+  <p>{{ tagline }}</p>
+
+  <div class="card">
+    <button class="login" type="button" @click="login()">Log into network</button>
+    <button class="join" type="button" @click="join()">Join the network</button>
   </div>
+
+  <p>
+    Check out the
+    <a href="https://github.com/sannidhyaroy/Ankaa" target="_blank">GitHub repository</a>
+    of Ankaa to learn more.
+  </p>
+  <p class="license">
+    Ankaa is licensed under the
+    <a
+      href="https://github.com/sannidhyaroy/Ankaa/blob/main/LICENSE"
+      target="_blank"
+      class="license-link"
+      >GNU General Public License (GPL) v3.0</a
+    >
+    license.
+  </p>
 </template>
 
 <style scoped>
-h1 {
-  font-weight: 500;
-  font-size: 2.6rem;
-  position: relative;
-  top: -10px;
+.card {
+  display: flex;
+  gap: 0.6rem;
+  justify-content: center;
 }
-
-h3 {
-  font-size: 1.2rem;
+.join {
+  background: #2a4747;
 }
-
-.greetings h1,
-.greetings h3 {
-  text-align: center;
+.license {
+  color: #888;
 }
-
-@media (min-width: 1024px) {
-  .greetings h1,
-  .greetings h3 {
-    text-align: left;
-  }
+.license-link {
+  color: #646cffaa;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <a href="https://youtu.be/dQw4w9WgXcQ" target="_blank">
+      <img src="../assets/logo.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Ankaa" tagline="Bringing neighbours together - one task at a time" />
 </template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
