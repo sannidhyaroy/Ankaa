2bd20a55ab95b57dfdc84fcb3b4f35847b16de2c
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-04-13 03:48:09 +0530

    `feat`: custom alert component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@ import { supabase } from './lib/supabase'
 
 const session = ref()
 const router = useRouter()
-const message = ref('')
 
 onMounted(() => {
   supabase.auth.getSession().then(({ data }) => {
@@ -21,35 +20,9 @@ onMounted(() => {
       router.push('/dashboard') // Redirect to Dashboard on login
     }
   })
-
-  // Handle error messages from URL
-  const urlParams = new URLSearchParams(window.location.hash.substring(1))
-  const error = urlParams.get('error')
-  const errorDescription = urlParams.get('error_description')
-
-  if (error && errorDescription) {
-    message.value = decodeURIComponent(errorDescription.replace(/\+/g, ' '))
-  }
 })
 </script>
 
 <template>
-  <div v-if="message" class="message-container">
-    <p class="message-text">{{ message }}</p>
-  </div>
   <RouterView :key="$route.fullPath" />
 </template>
-
-<style scoped>
-.message-container {
-  max-width: 80vw;
-  margin: 0.5em auto;
-  padding: 0.3em;
-  border-radius: 1em;
-  background-color: rgba(255, 181, 181, 0.2);
-}
-.message-text {
-  text-align: center;
-  margin: 1em 0;
-}
-</style>
--- src/views/JoinView.vue
+++ src/views/JoinView.vue
@@ -1,9 +1,20 @@
 <script setup>
 import { ref } from 'vue'
 import { supabase } from '../lib/supabase'
+import Alert from '../components/Alert.vue'
 
 const loading = ref(false)
 const email = ref('')
+const message = ref({
+  text: '',
+  type: false,
+})
+
+const emailSuccess = () => {
+  message.value.type = true
+  message.value.text = 'Check your email for the login link!'
+  email.value = ''
+}
 
 const handleLogin = async () => {
   try {
@@ -12,10 +23,10 @@ const handleLogin = async () => {
       email: email.value,
     })
     if (error) throw error
-    alert('Check your email for the login link!')
+    emailSuccess()
   } catch (error) {
     if (error instanceof Error) {
-      alert(error.message)
+      message.value.text = error.message
     }
   } finally {
     loading.value = false
@@ -24,6 +35,7 @@ const handleLogin = async () => {
 </script>
 
 <template>
+  <Alert v-if="message.text" :message="message.text" :isSuccess="message.type" />
   <div class="join-view">
     <h1>Join the network</h1>
     <form @submit.prevent="handleLogin">
@@ -41,8 +53,8 @@ const handleLogin = async () => {
 <style scoped>
 .join-view {
   max-width: 400px;
-  margin: 0 auto;
-  padding: 20px;
+  margin: 2em auto;
+  padding: 2em;
   border: 1px solid #ccc;
   border-radius: 8px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
@@ -72,13 +84,8 @@ input {
 }
 
 button {
-  width: 100%;
-  padding: 10px;
   background-color: #2a4747;
-  color: white;
   border: none;
-  border-radius: 4px;
-  cursor: pointer;
 }
 
 button:hover {
