665815aad72ce9b00f91dcb4d46b18336f02ee03
Sannidhya <58990408+sannidhyaroy@users.noreply.github.com>
2025-04-12 09:37:21 +0530

    `feat`: redirect to dashboard if logged in

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { RouterView } from 'vue-router'
+import { RouterView, useRouter } from 'vue-router'
 import { supabase } from './lib/supabase'
 
 const session = ref()
+const router = useRouter()
 
 onMounted(() => {
   supabase.auth.getSession().then(({ data }) => {
     session.value = data.session
+    if (session.value) {
+      router.push('/dashboard') // Redirect to Dashboard if logged in
+    }
   })
 
   supabase.auth.onAuthStateChange((_, _session) => {
     session.value = _session
+    if (_session) {
+      router.push('/dashboard') // Redirect to Dashboard on login
+    }
   })
 })
 </script>
