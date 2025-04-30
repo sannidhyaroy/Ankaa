sannidhyaroy/Ankaa

commit ea2a2ddb24e95cc496286d4a0518253bff96b9ec

`fix`: show task creator name instead of id

database/functions.sql
@@ -132,7 +132,7 @@ create or replace function get_sorted_tasks_by_location (
   due_date timestamp,
   location geography (Point, 4326),
   assignee uuid,
-  created_by uuid,
+  created_by text,
   created_on timestamp,
   distance_km double precision
 ) as $$
@@ -146,7 +146,7 @@ begin
     t.due_date,
     t.location,
     t.assignee,
-    t.created_by,
+    taskcreator.full_name as created_by,
     t.created_on,
     accurate_distance_km(t.location, p.location) as distance_km
   from
@@ -170,6 +170,7 @@ begin
           accurate_distance_km(tasks.location, p.location) <= max_distance_km
         )
     ) t
+    left join profiles taskcreator on taskcreator.id = t.created_by
   order by
     (t.location is null),
     accurate_distance_km(t.location, p.location),

commit 00dd162c9d0253e3a275126d8566185f252e1054

`fix`: task owns broken by ea2a2dd

database/functions.sql
@@ -134,6 +134,7 @@ create or replace function get_sorted_tasks_by_location (
   assignee uuid,
   created_by text,
   created_on timestamp,
+  self_created boolean,
   distance_km double precision
 ) as $$
 begin
@@ -148,6 +149,7 @@ begin
     t.assignee,
     taskcreator.full_name as created_by,
     t.created_on,
+    t.created_by = auth.uid() as self_created,
     accurate_distance_km(t.location, p.location) as distance_km
   from
     (select * from profiles where profiles.id = auth.uid()) p,

src/views/DashboardView.vue
@@ -48,7 +48,7 @@ export default {
         description: task.description,
         created_on: new Date(task.created_on).toLocaleString(),
         created_by: task.created_by || 'Anonymous',
-        owns: task.created_by === user.value?.id,
+        owns: task.self_created,
         distance: task.distance_km,
       }))
 